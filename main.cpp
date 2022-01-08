fe94e67469521a914407bd5fa3828fe71ff90a4b
MiklosLaszlo <mikykunsagi@gmail.com>
2022-01-08 02:31:39 +0100

    Seguimiento archivos

---
+++ main.cpp
@@ -0,0 +1,6 @@
+//Subgrupo Azul: Irene, Gabriel, Nicolás y Gerardo
+#include <stdio.h>  // for printf
+#include <SQLAPI.h> // main SQLAPI++ header
+//#include 
+#include <iostream>
+#include <ctime>
