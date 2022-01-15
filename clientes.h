ee583567236df2f348277d2aa7e8edb15a0574ff
Irene Trigueros <48659055+Itrilor@users.noreply.github.com>
2022-01-15 17:45:00 +0100

    Update clientes.h

--- clientes.h
+++ clientes.h
@@ -7,20 +7,22 @@ class Cliente{
   protected:
   string nombre;
   string contrasena;
-  int telefono;
+  unsigned int telefono;
   string correo;
   char sexo;
   string fecha_nacimiento;
-  int tarjeta;
+  unsigned int tarjeta;
   
   public:
   
-  Cliente(string n, string c, int telf, string cor, char s, string f, int t);
+  Cliente(string n, string c, unsigned int telf, string cor, char s, string f, unsigned int t);
   
-  void IniciarSesion(int telf);
-  void FinalizarSesion(int id);
+  void IniciarSesion(unsigned int telf);
+  void FinalizarSesion(unsigned int id);
   void DarBajaCliente();
-  void ModificarCliente(string n, string c, string cor, char s, string f, int t);
+  void ModificarCliente(string n, string c, string cor, char s, string f, unsigned int t);
+  
+  bool TieneNDigitos();
 };
 
 
