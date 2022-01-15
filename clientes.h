80a751f5d04341f5050e1d4003f2545a9a014154
Irene Trigueros <48659055+Itrilor@users.noreply.github.com>
2022-01-15 17:14:29 +0100

    Update clientes.h

--- clientes.h
+++ clientes.h
@@ -1,1 +1,14 @@
+#ifndef CLIENTES_H
+#define CLIENTES_H
 
+class Cliente{
+  Cliente(string n, string c, int telf, string correo, char s, string fecha, int t);
+  
+  void IniciarSesion(int telf);
+  void FinalizarSesion(int id);
+  void DarBajaCliente();
+  void ModificarCliente();
+};
+
+
+#endif // ESCENA_H
