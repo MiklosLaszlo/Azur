8a8cc5836dba2150596473267836d6236ac4fe05
Irene Trigueros <48659055+Itrilor@users.noreply.github.com>
2022-01-15 20:37:11 +0100

    Update clientes.h

--- clientes.h
+++ clientes.h
@@ -22,7 +22,7 @@ void IniciarSesion(unsigned int telf, SAConnection* con);
 
 void FinalizarSesion(unsigned int id, SAConnection* con);
 
-void DarBajaCliente(SAConnection* con);
+void DarBajaCliente(unsigned int telf, SAConnection* con);
 
 void ModificarCliente(string n, string c, string cor, char s, string f, unsigned int t,SAConnection* con);
 //};
