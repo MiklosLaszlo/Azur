cf95c27d29d7fb7a94e5095f74ab0078d52f5d4a
Irene Trigueros <48659055+Itrilor@users.noreply.github.com>
2022-01-15 17:38:05 +0100

    Update clientes.cpp

--- clientes.cpp
+++ clientes.cpp
@@ -1,1 +1,23 @@
+#include "clientes.h"
 
+using namespace std;
+
+Cliente::Cliente(){
+  
+}
+
+void Cliente::IniciarSesion(){
+  
+}
+
+void Cliente::FinalizarSesion(){
+  
+}
+
+void Cliente::DarBajaCliente(){
+  
+}
+
+void Cliente::ModificarCliente(){
+  
+}
