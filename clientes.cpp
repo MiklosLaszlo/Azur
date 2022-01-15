c7236bea930fafc52671f7d67b981129f6015734
Irene Trigueros <48659055+Itrilor@users.noreply.github.com>
2022-01-15 20:01:39 +0100

    Update clientes.cpp

--- clientes.cpp
+++ clientes.cpp
@@ -34,7 +34,34 @@ Cliente::Cliente(string n, string c, unsigned int telf, string cor, char s, stri
   crear.Param(2).setAsString() = cor;
   crear.Param(2).setAsChar() = s;
   crear.Param(2).setAsString() = f;
+  
+  try{
+    crear.Execute();
+  }
+  catch(SAException &x){
+    int i = x.ErrNativeCode();
+    switch(i){
+      case 1:
+        cout << "Ya existía un cliente con ese teléfono, use ModificarCliente si quiere cambiar el nombre\n";
+        break;
+      case ___:
+        cout << "ERROR NOT NULLS\n";
+        break;
+      /*case 20001:
+        cout << "\n";
+        break;*/
+      default:
+        cout << "Excepción no controlada\n" << endl;
+        cout << x-ErrText().GetMultiByteChars()<< endl;
+        break;
+    }
+    
+    guardado.setCommandText(_TSA("ROLLBACK TO SAVEPOINT registrarcliente"));
+    guardado.Execute();
+    return ;
+  }
   //}
+  con->commit();
 }
 
 void Cliente::IniciarSesion(){
