20665de7afe7f2c9a423cfca332e2b88608dbdd1
Irene Trigueros <48659055+Itrilor@users.noreply.github.com>
2022-01-15 18:03:31 +0100

    Update clientes.cpp

--- clientes.cpp
+++ clientes.cpp
@@ -1,9 +1,15 @@
+#include <stdio.h>  // for printf
+#include <SQLAPI.h> // main SQLAPI++ header
 #include "clientes.h"
+#include <iostream>
 
 using namespace std;
 
-Cliente::Cliente(){
-  
+Cliente::Cliente(string n, string c, int telf, string cor, char s, string f, int t){
+  if(n.length()<50 && c.length()<50 && to_string(telf).length()==9 && cor.length()<50 
+     && (s=='F' || s=='M' || s=='O') && f.length()<50 && to_string(t).length()==16){
+    
+  }
 }
 
 void Cliente::IniciarSesion(){
