1445e77e253e14b04131cc8063a56480e7fdbd97
Gerardo2000 <44905696+Gerardo2000@users.noreply.github.com>
2022-01-21 20:50:00 +0100

    Update peliculas.h

--- peliculas.h
+++ peliculas.h
@@ -12,7 +12,7 @@ using namespace std;
 void MostrarCatalogo (SAConnection* con);
 void SuministrarPelicula (string t, int a, string d, string p, int CIF, SAConnection* con);
 void InhabilitarPelicula (int idP, SAConnection* con);
-void BuscarTituloCatalogo (string t, SAConnection *con);d
+void BuscarTituloCatalogo (string t, SAConnection *con);
 void MostrarRecomendaciones (int telefono,SAConnection *con);
 void VerPelicula (int idSesion, int idPelicula, SAConnection *con);
   
