8c97b21b5bcfb3640bb9e49a5bc98dcf417fd423
MiklosLaszlo <75449050+MiklosLaszlo@users.noreply.github.com>
2022-01-21 17:35:28 +0100

    Añadido disparadores

--- Prueba Disparadores.sql
+++ Prueba Disparadores.sql
@@ -176,6 +176,43 @@ BEGIN
 END comprobarRecomendacionInsertar;
 /
 
+CREATE OR REPLACE TRIGGER insertar_ContratoCliente --Crea id contrato cliente y comprueba que el cliente es activo
+  before insert on firmaClienteContratoCliente
+  for each row
+    DECLARE
+      cli INTEGER
+    BEGIN
+      SELECT COUNT(*) INTO cli FROM ClienteActivo WHERE telefono=new.telefono; --el tlf que quiero insertar
+      IF (cli<1) THEN
+        RAISE_APPLICATION_ERROR(-20100, 'INTENTO DE CREAR CONTRATO A UN CLIENTE NO ACTIVO')
+      END IF
+      SELECT secuencia_contratoCliente.nextval INTO :new.idContratoCliente FROM dual;
+    END insertar_ContratoCliente;
+/
+
+CREATE OR REPLACE TRIGGER insertar_ContratoProveedor --Crea id contrato proveedor
+  before insert on firmaProveedorContratoProveedor
+  for each row
+    BEGIN
+      SELECT secuencia_contratoProveedor.nextval INTO :new.idContratoProveedor FROM dual;
+    END insertar_ContratoProveedor;
+/
+
+CREATE OR REPLACE TRIGGER insertar_FacturaCliente --Crea id factura cliente
+  before insert on facturaClientePaga
+  for each row
+    BEGIN
+      SELECT secuencia_facturaCliente.nextval INTO :new.idfacturac FROM dual;
+    END insertar_FacturaCliente;
+/
+
+CREATE OR REPLACE TRIGGER insertar_FacturaProveedor --Crea id factura proveedor
+  before insert on facturaProveedorRecibeDinero
+  for each row
+    BEGIN
+      SELECT secuencia_facturaProveedor.nextval INTO :new.idfacturap FROM dual;
+    END insertar_FacturaProveedor;
+/
 
 CREATE OR REPLACE TRIGGER comprobarPeliculaInsertada
 BEFORE INSERT ON PELICULA
